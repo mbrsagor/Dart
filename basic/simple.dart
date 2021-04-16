69fae4f215767977279b8909b7cc4722c2c57915
Md.Bozlur Rosid Sagor <39002174+mbrsagor@users.noreply.github.com>
2021-04-16 22:14:06 +0600

    Create simple.dart

---
+++ basic/simple.dart
@@ -0,0 +1,17 @@
+void aboutMe() {
+  String first_name = "Bozlur Rosid";
+ String last_name = "Sagor";
+ String full_name = first_name+ " "+ last_name;
+  return full_name;
+}
+
+void stack() {
+  var lst = new List(3); 
+   lst[0] = "Python"; 
+   lst[1] = "Javascript"; 
+   lst[2] = "Dart"; 
+}
+
+void main() {
+  aboutMe();
+}
